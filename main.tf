7f07b38f5167573f0fc6b9e29955221efcf1128d
aafetorgbor <85805640+aafetorgbor@users.noreply.github.com>
2023-07-12 14:19:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ data "ibm_resource_group" "group" {
 }
 
 resource "ibm_code_engine_project" "code_engine_project_instance" {
-  name              = "terraform-test-4"
+  name              = "terraform-test-5"
   resource_group_id = data.ibm_resource_group.group.id
 }
 
